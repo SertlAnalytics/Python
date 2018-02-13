088d2a2c51858e738705854e9069ac48063cef25
Josef Sertl <26519550+JWSertl@users.noreply.github.com>
2018-02-13 15:30:08 +0100

    With printing

--- Week_02/driver_3.py
+++ Week_02/driver_3.py
@@ -1,13 +1,18 @@
 # ColumbiaX: CSMM.101x - Assigment week 2
-# Search algorithms - Josef Sertl
+# Search algorithms - 2018-02-01
+# Copyright Josef Sertl
 
 import math
+import time
+import os
+import psutil
 
 class SwitchDirection:
     UP = 'Up'
     DOWN = 'Down'    
     LEFT = 'Left'
     RIGHT = 'Right'
+    NONE = 'None'
 
 class Stack: #Last in - first out LIFO
     def __init__(self):
@@ -48,47 +53,79 @@ class Queue: #First in - first out FIFO
         return len(self.items)
     
 class BoardTree:
-    def __init__(self, initialBoard, goalBoard):
-        self.initialBoard = initialBoard
-        self.goalBoard = goalBoard
+    def __init__(self, initial_board, goal_board):
+        self.initial_board = initial_board
+        self.goal_board = goal_board
         self.explored = []
-        self.frontierItems = []
+        self.frontier_items = []
+        self.success_board = None
+        self.success_path = []
+        self.nodes_expanded = 0
+        self.max_search_depth = 0
     
-    def performBFS(self, inputBoard):
+    def performBFS(self, input_board):
         frontier = Queue()        
-        frontier.enqueue(inputBoard)
-        self.frontierItems.append(inputBoard.items)
+        frontier.enqueue(input_board)
+        self.frontier_items.append(input_board.items)
         
         while not frontier.isEmpty():
             board = frontier.dequeue()
-            board = Board(*board.items)            
             self.explored.append(board)
-                        
-            if board.isIdentical(self.goalBoard):
+            
+            if board.isIdentical(self.goal_board):
                 print('BOARD is identical to GOAL')
+                self.success_board = board
+                self.success_path = self.get_success_path()
                 return True
             
             neighborBoardList = board.getNeighborBoards()
+            self.nodes_expanded += 1
+            if self.max_search_depth < board.level_in_searchtree:
+                self.max_search_depth = board.level_in_searchtree + 1
                             
-            for neighbors in neighborBoardList:
-                if not neighbors in self.explored and not neighbors.items in self.frontierItems:
+            for neighbors in neighborBoardList:               
+                if not neighbors in self.explored and not neighbors.items in self.frontier_items:
                     frontier.enqueue(neighbors)
-                    self.frontierItems.append(neighbors.items)
+                    self.frontier_items.append(neighbors.items)
                                 
-    def printPath(self):
+    def printPathExplored(self):
         counter = 0
         for boards in self.explored:
             counter += 1
             boards.printMatrix('Explored ' + str(counter))
         self.goalBoard.printMatrix('Goal')
         
+    def print_success_path(self):       
+        for boards in self.success_path:
+            boards.printMatrix('Success path - created by switch: ' + boards.direction_from_parent) 
+            
+    def get_path_to_goal(self):
+        path_to_goal = []        
+        for boards in self.success_path:
+            if boards.direction_from_parent != SwitchDirection.NONE:
+                path_to_goal.append(boards.direction_from_parent)
+        return path_to_goal
+            
+    def get_success_path(self):
+        success_path_inverted = []
+        success_path_inverted.append(self.success_board)
+        parent_board = self.success_board.parent_board
+        while not parent_board is None: 
+            success_path_inverted.append(parent_board)
+            parent_board = parent_board.parent_board
+        return success_path_inverted[::-1] #.reverse()
+        
 class Board:
+    
     def __init__(self, *args):
         self.items = []    
         for p in args:
             self.items.append(p)
         self.dim = int(math.sqrt(len(self.items)))
-            
+        self.level_in_searchtree = 1
+        self.parent_board = None
+        self.direction_from_parent = SwitchDirection.NONE
+             
     def getMatrix(self):
         self.matrix = []       
         row = []
@@ -129,7 +166,12 @@ class Board:
         if self.isSwitchZeroPossible(direction) == False:
             return None
         newBoard = self.clone()
+        newBoard.parent_board = self
+        newBoard.direction_from_parent = direction
+        newBoard.level_in_searchtree = self.level_in_searchtree + 1
         newBoard.switchZero(direction)
+#        if not newBoard.parent_board is None:
+#            newBoard.parent_board.printMatrix('getNeighborBoard.parent_board')
         return newBoard
         
     def switchZero(self, direction): #Up, Down, Left, Right UDLR        
@@ -167,20 +209,69 @@ class Board:
               return True
         return False        
         
-args = (1,2,0,3,4,5,6,7,8)
-args = (1,2,5,3,4,0,6,7,8) #example 1
+class Solver:
+    def __init__(self, tree_type, *args):
+        self.tree_type = tree_type
+        self.board_start = Board(*args)
+        self.board_goal = Board(0,1,2,3,4,5,6,7,8)
+        self.board_tree = BoardTree(self.board_start, self.board_goal)
+        self.running_start = time.time()
+        self.running_end = None
+        
+                     
+    def run_algorithm(self):
+        self.board_tree.performBFS(self.board_start)
+        self.running_end = time.time()
 
-argsGoal = (0,1,2,3,4,5,6,7,8)
-board = Board(*args)
-boardGoal = Board(*argsGoal)
-
-board.printMatrix('Start')
-boardGoal.printMatrix('Goal')
-
-boardTree = BoardTree(board, boardGoal)
-boardTree.performBFS(board)
-boardTree.printPath()
+    def print_results(self):
+#        self.board_tree.print_success_path()
+        for entries in self.get_results():
+            print(entries)
+            
+    def write_results(self):
+        with open('output.txt', 'w') as fobj:
+            for entries in self.get_results():
+               fobj.write(entries + '\n')
+     
+    def get_results(self):
+        value_list = []
+        value_list.append('path_to_goal: {}'.format(str(self.board_tree.get_path_to_goal())))
+        value_list.append('cost_of_path: {}'.format(len(self.board_tree.success_path)-1))
+        value_list.append('notes_expanded: {}'.format(self.board_tree.nodes_expanded))
+        value_list.append('search_depth: {}'.format(len(self.board_tree.success_path)-1))
+        value_list.append('max_search_depth: {}'.format(self.board_tree.max_search_depth))
+        value_list.append('running_time: {0:9.8f}'.format(self.running_start - self.running_end))
+        value_list.append('max_ram_usage: {0:9.8f}'.format(self.get_max_ram_usage()))   
+        return value_list
+        
+    def get_max_ram_usage(self):
+        process = psutil.Process(os.getpid())
+#        return process.memory_info().rss
+        return process.memory_info()[0] # float(2 ** 20)
     
+args = (1,2,5,3,4,0,6,7,8)
+solver = Solver('BFS', *args)
+solver.run_algorithm()
+solver.print_results()
+solver.write_results()
+
+#args = (1,2,0,3,4,5,6,7,8)
+#args = (1,2,5,3,4,0,6,7,8) #example 1
+#
+#args_goal = (0,1,2,3,4,5,6,7,8)
+#board_start = Board(*args)
+#board_goal = Board(*args_goal)
+#
+#board_start.printMatrix('Start')
+#board_goal.printMatrix('Goal')
+#
+#board_tree = BoardTree(board_start, board_goal)
+#board_tree.performBFS(board_start)
+#board_tree.success_board.printMatrix('console: success_board') 
+#board_tree.success_board.parent_board.printMatrix('console: success_board.parent_board') 
+##boardTree.printPathExplored()
+#board_tree.print_success_path()
+
 
 
 """  
